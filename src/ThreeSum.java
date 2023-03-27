13d7a2775689b5563f896b335d3b9c517e40a28c
bitflame <monazzam@protonmail.com>
2023-03-27 12:34:04 -0400

    fixed a minor error in the UniquePathsII class

---
+++ src/ThreeSum.java
@@ -0,0 +1,6 @@
+public class ThreeSum {
+    // ThreeSum, Four Sum, and k-sum
+    public static void main(String[] args) {
+        System.out.println("Testing");
+    }
+}
--- src/UniquePathsII.java
+++ src/UniquePathsII.java
@@ -25,9 +25,9 @@ public class UniquePathsII {
     }
 
     public void printArray(int[][] obstacleGrid) {
-        for (int i = 0; i < obstacleGrid.length; i++) {
+        for (int[] ints : obstacleGrid) {
             for (int j = 0; j < obstacleGrid[0].length; j++) {
-                System.out.print(obstacleGrid[i][j]);
+                System.out.print(ints[j] + " ");
             }
             System.out.println();
         }
@@ -54,6 +54,30 @@ public class UniquePathsII {
         return obstacleGrid[m - 1][n - 1];
     }
 
+    public int testing(int[][] obstacleGrid) {
+        if (obstacleGrid[0][0] == 1) return 0;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        obstacleGrid[0][0] = 1;
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1) {
+                obstacleGrid[i][0] = 1;
+            } else obstacleGrid[i][0] = 0;
+        }
+        for (int i = 1; i < n; i++) {
+            if (obstacleGrid[0][i] == 0 && obstacleGrid[0][i - 1] == 1) obstacleGrid[0][i] = 1;
+            else obstacleGrid[0][i] = 0;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+                } else obstacleGrid[i][j] = 0;
+            }
+        }
+        return obstacleGrid[m - 1][n - 1];
+    }
+
     public static void main(String[] args) {
         int[][] arr;
         arr = new int[][]{{1, 0, 1}, {0, 0, 0}, {0, 1, 0}};
@@ -67,15 +91,34 @@ public class UniquePathsII {
         System.out.println("Test 1 - Result of uniquePaths(), Expecting 2, Getting : " + uniquePathsII.calculateUniquePaths(obstacleGrid));
         obstacleGrid = new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
         System.out.println("Test 1 - Result of 2nd Try, Expecting 2, Getting : " + uniquePathsII.secondTry(obstacleGrid));
+        obstacleGrid = new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+        System.out.println("Test 1 - Result of testing(), Expecting 2, Getting : " + uniquePathsII.testing(obstacleGrid));
         obstacleGrid = new int[][]{{0, 0}, {0, 1}};
         System.out.println(uniquePathsII.calculateUniquePaths(obstacleGrid));
         System.out.println("Test 2 - Result of uniquePaths(), Expecting 0, Getting : " + uniquePathsII.calculateUniquePaths(obstacleGrid));
         obstacleGrid = new int[][]{{0, 0}, {0, 1}};
         System.out.println("Test 2 - Result of 2nd Try, Expecting 0, Getting : " + uniquePathsII.secondTry(obstacleGrid));
+        obstacleGrid = new int[][]{{0, 0}, {0, 1}};
+        System.out.println("Test 3 - Result of testing(), Expecting 0, Getting : " + uniquePathsII.testing(obstacleGrid));
         obstacleGrid = new int[][]{{0}, {1}};
         System.out.println(uniquePathsII.calculateUniquePaths(obstacleGrid));
         System.out.println("Test 3 - Result of uniquePaths(), Expecting 0, Getting : " + uniquePathsII.calculateUniquePaths(obstacleGrid));
         obstacleGrid = new int[][]{{0}, {1}};
         System.out.println("Test 3 - Result of 2nd Try, Expecting 0 Getting : " + uniquePathsII.secondTry(obstacleGrid));
+        obstacleGrid = new int[][]{{0}, {1}};
+        System.out.println("Test 3 - Result of testing(), Expecting 0 Getting : " + uniquePathsII.testing(obstacleGrid));
+        int[][] foo = new int[3][4];
+        // create a two-dimensional array of 3 rows and 4 columns
+        // and assign 1 to all the items in the first row
+        for (int i = 0; i < foo[0].length; i++) {
+            foo[0][i] = 1;
+        }
+        System.out.println();
+        // now assign 1 to all the items in the first column
+        for (int i = 0; i < foo.length; i++) {
+            foo[i][0] = 1;
+        }
+        uniquePathsII.printArray(foo);
+        System.out.println();
     }
 }
