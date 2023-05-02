6e98d92b31ba494fa49cda01824ea5761f726765
bitflame <monazzam@protonmail.com>
2023-05-02 19:05:31 -0400

    added another method called findMedian

--- src/FindMedianSortedArray.java
+++ src/FindMedianSortedArray.java
@@ -8,7 +8,7 @@ public class FindMedianSortedArray {
 
         if (len1 != 0) {
             if (nums1[len1 - 1] < nums2[0]) {
-                return (double) (nums1[len1] + nums2[0]) / 2;
+                return (double) (nums1[len1 - 1] + nums2[0]) / 2;
             } else if (nums2[len2 - 1] < nums1[0]) return (double) (nums2[len2 - 1] + nums1[0]) / 2;
             if (len1 % 2 == 0) {
                 med1 = (double) (nums1[len1 / 2] + nums1[(len1 / 2) - 1]) / 2;
@@ -39,38 +39,55 @@ public class FindMedianSortedArray {
         int len1 = nums1.length;
         int len2 = nums2.length;
         int totalLen = len1 + len2;
-        while (a < len1 && b < len2) {
-            if (nums1[a] < nums2[b]) a++;
-            else b++;
+        int mid = 0, nextToMid = 0;
+        if (len1 > 0) mid = nums1[a];
+        if (len2 > 0) mid = nums2[b];
+        while (a < len1 && a < len2) {
+            if (nums1[a] < nums2[a]) {
+                mid = nums1[a];
+                nextToMid = nums2[a];
+
+            } else {
+                mid = nums2[a];
+                nextToMid = nums1[a];
+            }
+            a++;
         }
         if (totalLen % 2 == 0) {
-            return (nums1[a] + nums2[b]) / 2;
+            return (double) (mid + nextToMid) / 2;
         } else {
-            return nums1[a];
+            return mid;
         }
     }
+
 
     public static void main(String[] args) {
         FindMedianSortedArray findMedianSortedArray = new FindMedianSortedArray();
         int[] nums1 = {1, 3};
         int[] nums2 = {2};
-        System.out.println("Expected Value: 2.0; Actual Value: " + findMedianSortedArray.findMedianSortedArray(nums1, nums2));
-        System.out.println("Result of findMedian - Expected Value: 2.0; Actual Value: " + findMedianSortedArray.findMedian(nums1, nums2));
+        // System.out.println("Expected Value: 2.0; Actual Value: " + findMedianSortedArray.findMedianSortedArray(nums1, nums2));
+        System.out.println("Test 1 - Result of findMedian - Expected Value: 2.0; Actual Value: " + findMedianSortedArray.findMedian(nums1, nums2));
         nums1 = new int[]{1, 2};
         nums2 = new int[]{3, 4};
-        System.out.println("Expected Value: 2.5; Actual Value: " + findMedianSortedArray.findMedianSortedArray(nums1, nums2));
-        System.out.println("Result of findMedian - Expected Value: 2.5; Actual Value: " + findMedianSortedArray.findMedian(nums1, nums2));
+        // System.out.println("Expected Value: 2.5; Actual Value: " + findMedianSortedArray.findMedianSortedArray(nums1, nums2));
+        System.out.println("Test 2 - Result of findMedian - Expected Value: 2.5; Actual Value: " + findMedianSortedArray.findMedian(nums1, nums2));
         nums1 = new int[]{0, 0};
         nums2 = new int[]{0, 0};
-        System.out.println("Expected Value: 0.0; Actual Value: " + findMedianSortedArray.findMedianSortedArray(nums1, nums2));
-        System.out.println("Result of findMedian - Expected Value: 0.0; Actual Value: " + findMedianSortedArray.findMedian(nums1, nums2));
+        // System.out.println("Expected Value: 0.0; Actual Value: " + findMedianSortedArray.findMedianSortedArray(nums1, nums2));
+        System.out.println("Test 3 - Result of findMedian - Expected Value: 0.0; Actual Value: " + findMedianSortedArray.findMedian(nums1, nums2));
         nums1 = new int[]{};
         nums2 = new int[]{1};
-        System.out.println("Expected Value: 1.0; Actual Value: " + findMedianSortedArray.findMedianSortedArray(nums1, nums2));
-        System.out.println("Result of findMedian - Expected Value: 1.0; Actual Value: " + findMedianSortedArray.findMedian(nums1, nums2));
+        // System.out.println("Expected Value: 1.0; Actual Value: " + findMedianSortedArray.findMedianSortedArray(nums1, nums2));
+        System.out.println("Test 4 - Result of findMedian - Expected Value: 1.0; Actual Value: " + findMedianSortedArray.findMedian(nums1, nums2));
         nums1 = new int[]{2, 7};
         nums2 = new int[]{1, 3};
-        System.out.println("Expected Value: 2.5; Actual Value: " + findMedianSortedArray.findMedianSortedArray(nums1, nums2));
-        System.out.println("Result of findMedian - Expected Value: 2.5; Actual Value: " + findMedianSortedArray.findMedian(nums1, nums2));
+        // System.out.println("Expected Value: 2.5; Actual Value: " + findMedianSortedArray.findMedianSortedArray(nums1, nums2));
+        System.out.println("Test 5 - Result of findMedian - Expected Value: 2.5; Actual Value: " + findMedianSortedArray.findMedian(nums1, nums2));
+        nums1 = new int[]{1};
+        nums2 = new int[]{};
+        System.out.println("Test 6 - Result of findMedian - Expected Value: 1.0; Actual Value: " + findMedianSortedArray.findMedian(nums1, nums2));
+        nums1 = new int[]{1, 2, 3, 7};
+        nums2 = new int[]{};
+        System.out.println("Test 7 - Result of findMedian - Expected Value: 2.5; Actual Value: " + findMedianSortedArray.findMedian(nums1, nums2));
     }
 }
